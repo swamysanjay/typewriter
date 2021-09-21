f4a6c070c5ddb8bfb212facea514242cc85381c3
Sanjay Swamy <swamysanjay98@gmail.com>
2021-09-21 19:14:37 +0000

    Created a file where console log is animated

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs \n";
+
+let delay = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    // print the char here
+    process.stdout.write(char);
+  }, delay);
+  delay += 50; 
+};
+
